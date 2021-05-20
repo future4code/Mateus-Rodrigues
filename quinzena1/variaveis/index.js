b792f1ba3cfac1ae020bce25c96350be8745ad5b
Mateus Rodrigues <macmaquina@Meu-Mac.broadband>
2021-05-20 23:14:50 +0100

    JS - Variaveis

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,72 @@
+// Comentario de Linha
+
+/* Comentario de Bloco */
+/*de codigo
+linha de codigo
+linha de codigo
+linha de*/ 
+
+// console.log(" Olá Mundo ! ")
+// console.log("Turma Jonhson")
+
+// const Boooora = prompt ("Vamos nessa ?") 
+
+// console.log("Boooora")
+
+// variaveis const
+//const novaVariavel = 23 // apareceu check
+
+//novaVariavel = 24 // isso da erro
+
+//console.log (novaVariavel) // nao aparece
+
+// variaveis let
+
+// variaveis let
+
+// let novaVariavel = 24
+
+// //console.log (novaVariavel) // apareceu check
+
+// const novaVariavel = 23
+
+// console.log com varios argumentos
+
+// const nome = "Mateus"
+// const idade = 28
+
+// console.log("Meu nome é" nome "eu tenho" idade "anos")
+
+// Exercicio 1
+// const nome = "Mateus"
+// const sobrenome = "Rodrigues"
+// let idade = null
+// const checkEstudante = false
+
+// console.log("Meu nome é", nome, sobrenome, ", eu tenho", idade, "anos.", "Status de estudante:", checkEstudante)
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof checkEstudante)
+
+// Exercicio 2
+
+// const nome = prompt("Qual o seu nome ?")
+// const idade = prompt("Qual a sua idade ?")
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// const numeroAleatorio = 07
+// console.log(typeof numeroAleatorio)
+// console.log(typeof numeroAleatorio)
+
+// const numeroAleatorioEmTexto = numeroAleatorio.toString()
+// console.log(numeroAleatorioEmTexto)
+// console.log(typeof numeroAleatorioEmTexto)
+
+const nome = prompt("Qual o seu nome ?")
+const idade = Number ("Qual a sua idade ?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
