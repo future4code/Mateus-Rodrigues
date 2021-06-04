d0a3b7fe44b1288a80227f994e5211cd57049697
Mateus Rodrigues <macmaquina@Meu-Mac.broadband>
2021-06-04 14:43:30 +0100

    Projeto Quinzea1

---
+++ quinzena1/funcoes/index.js
@@ -0,0 +1,10 @@
+// Exercicio 1 de interpretação
+
+// ( A ) Vai ser impresso no console a multiplicação do numeros 2*5 = 10 e 10*5 = 50.
+// ( B ) Não aconteceria nada, para isso precisariamos do 
+
+
+// Exercicio 2 de interpretação
+
+// ( A ) Ela pede um texto ao usuario
+
