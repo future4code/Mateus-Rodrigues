4fa7b567eb0d45aff14a60882009dc4b4c4607f2
Mateus Rodrigues <macmaquina@Meu-Mac.broadband>
2021-06-21 00:49:30 +0100

    Questao 8 - Resolvida

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -81,6 +81,13 @@ function checaTriangulo(a, b, c) {
   // return 'Escaleno'
   // return 'Equilátero'
   // return 'Isósceles'
+  if (a===b&&b===c){
+    return 'Equilátero'
+  } else if (a===b||b===c||c===a){
+    return 'Isósceles'
+  } else {
+    return ('Escaleno')
+  }
 }
 
 // EXERCÍCIO 09
