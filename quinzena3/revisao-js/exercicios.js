8e2a8458b81502e0511d5e30c34f2371efef8c1b
Mateus Rodrigues <macmaquina@Meu-Mac.broadband>
2021-06-21 01:02:16 +0100

    Questao 14 - Resolvida

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -164,7 +164,13 @@ function imprimeChamada() {
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
-
+  const retangulo = {
+    largura: lado1,
+    altura: lado2,
+    perimetro: (2*Number(lado1+lado2)),
+    area: Number(lado1*lado2)
+  }
+  return retangulo
 }
 
 // EXERCÍCIO 15
